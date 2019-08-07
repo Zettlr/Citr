e19c7fe6785d216e7692794f233f18505c6c1437
Carlos Precioso <kalprestito@gmail.com>
2019-08-07 16:27:20 +0200

    Add declaration output

---
+++ dist/citr.d.ts
@@ -0,0 +1,17 @@
+import { validateCitationID } from "./util/validator";
+import { extractCitations } from "./util/extract-citations";
+export declare const util: {
+    "validateCitationID": typeof validateCitationID;
+    "extractCitations": typeof extractCitations;
+};
+interface Citation {
+    "prefix": string;
+    "suffix": string;
+    "id": string;
+    "locator": string;
+    "label": string;
+    "suppress-author": boolean;
+}
+export declare function parseSingle(citation: string): Citation[];
+export declare function makeCitation(citationArray: Citation[]): string;
+export {};
---
+++ dist/util/retrieve-locator.d.ts
@@ -0,0 +1,21 @@
+/*!
+ * BEGIN HEADER
+ *
+ * Contains:    Utility functions to retrieve the locator of citations.
+ * Maintainer:  Hendrik Erz
+ * License:     GNU GPL v3
+ *
+ * Description:     These are utility functions that help in retrieving the
+ *      locator from a given citation string. ATTENTION these
+ *      functions are not to be used outside of the Citr module
+ *      as they require a very specific input string format!
+ *
+ * END HEADER
+ */
+interface FullSuffix {
+    "locator": string;
+    "label": string;
+    "suffix": string;
+}
+export declare function extractLocator(afterKey: string): FullSuffix;
+export {};
